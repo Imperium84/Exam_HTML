42c38845c645330bd0290ce6e6a15938e57a1cc7
Antoine Cornwell <cornwell.a.euroa2016@gmail.com>
2019-05-29 15:04:06 +0200

    initial

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var hambMenu = document.getElementById("hambMenu");
+
+hambMenu.addEventListener("click", function(){
+   
+   var menuCacher = document.getElementById("menuCacher");
+   menuCacher.classList.toggle("show");
+    
+});
